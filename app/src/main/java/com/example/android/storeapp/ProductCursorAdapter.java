592274a482671c9f4026dc18cf0cfebe47033786
asmaa salama <asmaamilad@gmail.com>
2018-08-05 23:23:26 +0200

    Adding the Sale button to each list item in the Main Activity.

--- app/src/main/java/com/example/android/storeapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/storeapp/ProductCursorAdapter.java
@@ -5,12 +5,14 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import com.example.android.storeapp.data.StoreContract;
 
 public class ProductCursorAdapter extends CursorAdapter {
+    String productquantity;
 
     public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
@@ -24,11 +26,31 @@ public class ProductCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         TextView nameTextView = view.findViewById(R.id.name);
+        TextView priceTextView = view.findViewById(R.id.price);
+        final TextView quantityTextView = view.findViewById(R.id.quantity);
+        Button saleButton = view.findViewById(R.id.sale);
 
         int nameColumnIndex = cursor.getColumnIndex(StoreContract.StoreEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(StoreContract.StoreEntry.COLUMN_PRODUCT_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(StoreContract.StoreEntry.COLUMN_PRODUCT_QUANTITY);
 
         String productName = cursor.getString(nameColumnIndex);
+        String productprice = cursor.getString(priceColumnIndex);
+        productquantity = cursor.getString(quantityColumnIndex);
 
-        nameTextView.setText(productName);
+        nameTextView.setText("Product name: " + productName);
+        priceTextView.setText("Product price: " + productprice);
+        quantityTextView.setText("Product quantity: " + productquantity);
+
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int temp = Integer.parseInt(productquantity);
+                if (temp > 0) {
+                    temp--;
+                }
+                quantityTextView.setText("Product quantity: " + temp);
+                productquantity = String.valueOf(temp);
+            }
+        });
     }
 }
